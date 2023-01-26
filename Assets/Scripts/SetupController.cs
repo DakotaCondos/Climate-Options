c9c1ba1b9fe5970fbf37cef5306bd2f0308f028b
mainPC <dakota242424@live.com>
2023-01-26 14:28:45 -0800

    Updated setup scene UI hierarchy with a body and footer section. Body has a scroller component. Added field validation to zip and utilities. Added resonse block feedback for validation.

--- Assets/Scripts/SetupController.cs
+++ Assets/Scripts/SetupController.cs
@@ -3,6 +3,8 @@ using NovaSamples.UIControls;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
 
 public class SetupController : MonoBehaviour
@@ -10,11 +12,32 @@ public class SetupController : MonoBehaviour
     public bool autoCalculateUtilityRate;
     [SerializeField]
     private GameObject novaToggle;
+    [SerializeField]
+    private float scaleFadeTime = 0.2f;
     Toggle toggle;
+
+    [SerializeField]
+    TMP_Text zipTMP;
+
     [SerializeField]
     GameObject utilitiesBlock;
     [SerializeField]
-    private float scaleFadeTime = 0.2f;
+    TMP_Text electrictyTMP;
+    [SerializeField]
+    TMP_Text gasTMP;
+    [SerializeField]
+    TMP_Text oilTMP;
+    [SerializeField]
+    TMP_Text woodPelletTMP;
+
+    [SerializeField]
+    public TMP_Text responseTextblock;
+    private float electric = 0;
+    private float gas = 0;
+    private float oil = 0;
+    private float woodPellet = 0;
+
+
     private void Awake()
     {
         toggle = novaToggle.GetComponent<Toggle>();
@@ -39,7 +62,6 @@ public class SetupController : MonoBehaviour
         if (utilitiesBlock != null)
         {
             Scale(!autoCalculateUtilityRate);
-
         }
         else
         {
@@ -77,5 +99,76 @@ public class SetupController : MonoBehaviour
 
         //Ensure scale is performed correctly from lerp
         objectToScale.transform.localScale = destinationScale;
+    }
+
+    public void UpdateResponse(string msg, Color color)
+    {
+        responseTextblock.color = color;
+        responseTextblock.text = msg;
+    }
+
+    public void CollectUtilityData()
+    {
+        //error check input fields
+        if (!IsZipInputValid())
+            return;
+        if (!autoCalculateUtilityRate && !IsUtilityInputValid())
+            return;
+
+        // Call 
+        UpdateResponse("Input is valid", Color.green);
+    }
+
+    private bool IsZipInputValid()
+    {
+        if (string.IsNullOrWhiteSpace(zipTMP.text))
+        {
+            UpdateResponse("Zip Code is required", Color.red);
+            return false;
+        }
+
+        try
+        {
+            if (!Regex.IsMatch(zipTMP.text, @"^\d{5}"))
+            {
+                UpdateResponse("Zip Code must be a 5 digit number", Color.red);
+                return false;
+            }
+        }
+        catch (RegexMatchTimeoutException)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool IsUtilityInputValid()
+    {
+        if (!Single.TryParse(electrictyTMP.text, out electric))
+        {
+            UpdateResponse("Electricity must be a numerical value", Color.red);
+            return false;
+        }
+
+        if (!Single.TryParse(gasTMP.text, out gas))
+        {
+            UpdateResponse("Gas must be a numerical value", Color.red);
+            return false;
+        }
+
+        if (!Single.TryParse(oilTMP.text, out oil))
+        {
+            UpdateResponse("Oil must be a numerical value", Color.red);
+            return false;
+        }
+
+        if (!Single.TryParse(woodPelletTMP.text, out woodPellet))
+        {
+            UpdateResponse("Wood Pellet must be a numerical value", Color.red);
+            return false;
+        }
+
+        return true;
     }
 }
