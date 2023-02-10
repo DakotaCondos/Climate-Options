4aa2dd199b2a74ae37eb3ee30ec3c3e41f3fe013
mainPC <dakota242424@live.com>
2023-02-10 14:12:42 -0800

    Removed FloorConfig class. Added dynamic creation of UI buttons for each room in a HouseConfig. Rooms are labeled sequentially as "Room X" or "Bathroom X"

--- Assets/Scripts/RoomConfig.cs
+++ Assets/Scripts/RoomConfig.cs
@@ -3,24 +3,32 @@
 public class RoomConfig
 {
     public List<ClimateControlComponent> components;
-    // size in ft^3
-    private readonly float size;
-    public float Size { get => size; }
     public int roomNumber;
+    public bool isBathroom = false;
 
-    public RoomConfig(int roomNumber)
+    // size in ft^3
+    public float size;
+    // US average bedroom size is 200 ft^2 * 8 ft ceiling = 1600 ft^3
+    // US average bathroom size is 70 ft^2 * 8 ft ceiling = 560 ft^3
+    private float defaultBedroomSize = 1600;
+    private float defaultBathroomSize = 560;
+
+
+    public RoomConfig(int roomNumber, bool isBathroom)
     {
         components = new List<ClimateControlComponent>();
-        // US average room size is 200 ft^2 * 8 ft ceiling = 1600 ft^3
-        size = 1600;
         this.roomNumber = roomNumber;
+        this.isBathroom = isBathroom;
+
+        size = (isBathroom) ? defaultBathroomSize : defaultBedroomSize;
     }
 
-    public RoomConfig(List<ClimateControlComponent> components, float size, int roomNumber)
+    public RoomConfig(List<ClimateControlComponent> components, float size, int roomNumber, bool isBathroom)
     {
         this.components = components;
         this.size = size;
         this.roomNumber = roomNumber;
+        this.isBathroom = !isBathroom;
     }
 
 }
