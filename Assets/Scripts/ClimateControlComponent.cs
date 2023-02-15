1f70d8fe5ee527db9f1d243ecfd1fd18fe516d65
mainPC <dakota242424@live.com>
2023-02-15 11:34:02 -0800

    Refactored ClimateControlComponents

--- Assets/Scripts/ClimateControlComponent.cs
+++ Assets/Scripts/ClimateControlComponent.cs
@@ -7,54 +7,53 @@ public class ClimateControlComponent
     private readonly string description;
     private readonly string pros;
     private readonly string cons;
-    private readonly string[] prerequisiteComponents;
-    private readonly bool isParentOnlyComponent;
-    private readonly bool isChildOnlyComponent;
+    private readonly ClimateControlComponentTypes[] prerequisiteComponentType;
+    private readonly bool isWholeHomeComponent;
     private readonly bool isHeating;
     private readonly bool isCooling;
     private readonly float heatingBTUOutput;
     private readonly float coolingBTUOutput;
-    private readonly float heatingCost;
-    private readonly float coolingCost;
+    private readonly float heatingCostPerBTU;
+    private readonly float coolingCostPerBTU;
 
     public string Name => name;
     public string Description => description;
     public string Pros => pros;
     public string Cons => cons;
-    public bool IsParentOnlyComponent { get => isParentOnlyComponent; }
-    public bool IsChildOnlyComponent { get => isChildOnlyComponent; }
+    public bool IsWholeHomeComponent => isWholeHomeComponent;
     public bool IsHeating { get => isHeating; }
     public bool IsCooling { get => isCooling; }
-    public float HeatingOutput { get => heatingBTUOutput; }
-    public float CoolingOutput { get => coolingBTUOutput; }
-    public float HeatingCost { get => heatingCost; }
-    public float CoolingCost { get => coolingCost; }
-    public string[] PrerequisiteComponents { get => prerequisiteComponents; }
-
+    public float HeatingBTUOutput { get => heatingBTUOutput; }
+    public float CoolingBTUOutput { get => coolingBTUOutput; }
+    public float HeatingCostPerBTU { get => heatingCostPerBTU; }
+    public float CoolingCostPerBTU { get => coolingCostPerBTU; }
+    public ClimateControlComponentTypes[] PrerequisiteComponentType { get => prerequisiteComponentType; }
     public readonly ClimateControlComponentTypes componentType;
     public readonly UtilityType utilityType;
-
     public List<ClimateControlComponent> parentComponentsList = null;
 
-    public ClimateControlComponent(string name, string description, string pros, string cons, 
-        string[] prerequisiteComponents, bool isParentOnlyComponent, bool isChildOnlyComponent,
-        bool isHeating, bool isCooling, float heatingBTUOutput, float coolingBTUOutput, float heatingCost, 
-        float coolingCost, ClimateControlComponentTypes componentType, UtilityType utilityType)
+    public ClimateControlComponent(
+        string name, string description, string pros, string cons,
+        ClimateControlComponentTypes[] prerequisiteComponentType,
+        bool isWholeHomeComponent, bool isHeating, bool isCooling,
+        float heatingBTUOutput, float coolingBTUOutput, float heatingCostPerBTU,
+        float coolingCostPerBTU, ClimateControlComponentTypes componentType,
+        UtilityType utilityType, List<ClimateControlComponent> parentComponentsList)
     {
-        this.name = name ?? throw new ArgumentNullException(nameof(name));
-        this.description = description ?? throw new ArgumentNullException(nameof(description));
-        this.pros = pros ?? throw new ArgumentNullException(nameof(pros));
-        this.cons = cons ?? throw new ArgumentNullException(nameof(cons));
-        this.prerequisiteComponents = prerequisiteComponents ?? throw new ArgumentNullException(nameof(prerequisiteComponents));
-        this.isParentOnlyComponent = isParentOnlyComponent;
-        this.isChildOnlyComponent = isChildOnlyComponent;
+        this.name = name;
+        this.description = description;
+        this.pros = pros;
+        this.cons = cons;
+        this.prerequisiteComponentType = prerequisiteComponentType;
+        this.isWholeHomeComponent = isWholeHomeComponent;
         this.isHeating = isHeating;
         this.isCooling = isCooling;
         this.heatingBTUOutput = heatingBTUOutput;
         this.coolingBTUOutput = coolingBTUOutput;
-        this.heatingCost = heatingCost;
-        this.coolingCost = coolingCost;
+        this.heatingCostPerBTU = heatingCostPerBTU;
+        this.coolingCostPerBTU = coolingCostPerBTU;
         this.componentType = componentType;
         this.utilityType = utilityType;
+        this.parentComponentsList = parentComponentsList;
     }
 }
