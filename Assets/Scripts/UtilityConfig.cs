0d48fd602699d3b4776cbb443875c1fadeb0e68e
mainPC <dakota242424@live.com>
2023-02-27 12:50:24 -0800

    Added paramaterless constructor for utilconfig

--- Assets/Scripts/UtilityConfig.cs
+++ Assets/Scripts/UtilityConfig.cs
@@ -6,10 +6,18 @@ public class UtilityConfig
     public UtilityRates utilityrates;
     public int zip;
 
+    public UtilityConfig()
+    {
+        utilityrates = new UtilityRates();
+        zip = 99004;
+    }
+
     public UtilityConfig(UtilityRates utilityrates, int zip)
     {
         this.utilityrates = utilityrates;
         this.zip = zip;
     }
+
+
 
 }
